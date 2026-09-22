41c6866d4eb176d9c241bedfcb92687ac86dc950
Marcus Richmond <marcusrichmond.personal@gmail.com>
2026-09-21 19:02:45 -0500

    setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <main>
+            <h1>Wisconsin PFAS Dashboard</h1>
+            <p>Explore Wisconsin PFAS sampling locations.</p>
+        </main>
+    ) 
+}
+
+export default App;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("The root element was not found.");
+}
+
+const root = createRoot(rootElement);
+root.render(<App />);
