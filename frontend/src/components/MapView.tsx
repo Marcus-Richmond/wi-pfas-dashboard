f29951c5202ba1bd02e067aa8cb5a798080666db
Marcus Richmond <marcusrichmond.personal@gmail.com>
2026-09-21 20:25:56 -0500

    initialize maplibre

--- frontend/src/components/MapView.tsx
+++ frontend/src/components/MapView.tsx
@@ -1,7 +1,30 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import { Map, setWorkerUrl } from "maplibre-gl";
+import workerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url";
+
+setWorkerUrl(workerUrl);
 
 function MapView () {
     const mapContainerRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const container = mapContainerRef.current;
+
+        if (!container) {
+            return;
+        }
+
+        const map = new Map({
+            container: container,
+            style: "https://demotiles.maplibre.org/style.json",
+            center: [-89.5, 44.5],
+            zoom: 5.5,
+        });
+
+        return () => {
+            map.remove();
+        };
+    }, []);
 
     return (
         <div
